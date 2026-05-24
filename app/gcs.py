b01c1185515f00dd9945759a177acab2aa6ef6b8
Feli Martinet <fefemar04@gmail.com>
2026-05-24 17:44:39 +0200

    fix: correction syntaxe gcs.py imports mal placés

--- app/gcs.py
+++ app/gcs.py
@@ -1,3 +1,17 @@
+from google.cloud import storage
+import os
+import json
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+FILE_NAME = os.getenv("FILE_NAME")
+
+def read_json_from_gcs():
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(FILE_NAME)
+    content = blob.download_as_text()
+    return json.loads(content)
+
 def write_to_gcs(new_entry: dict):
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
@@ -13,19 +27,4 @@ def write_to_gcs(new_entry: dict):
     blob.upload_from_string(
         json.dumps(data, indent=2),
         content_type="application/json"
-    )from google.cloud import storage
-import os
-import json 
-
-BUCKET_NAME = os.getenv("BUCKET_NAME" )
-FILE_NAME = os.getenv("FILE_NAME")
-
-def read_json_from_gcs():
-    client = storage.Client()
-    bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blob(FILE_NAME)
-
-    content = blob.download_as_text()
-    return json.loads(content)
-
-
+    )
